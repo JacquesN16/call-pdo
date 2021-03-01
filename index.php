ac6ef388b83912626fac4dacefc06f84512ad916
JacquesNg <68400951+JacquesN16@users.noreply.github.com>
2021-03-01 20:41:10 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,24 +2,18 @@
 
 // ----  Chargement des fichiers de fonctions (librairie)
 include 'lib/database.php';
-
-
-
 // ---- Code principal
 // etape 1 init PDO 
 $pdo= connectToDatabase();
-
 //etape 2 préparation requête
 $query= $pdo->prepare("
 SELECT Nom,Description,Photo_url,Prix_indicatif 
 FROM `cocktail`; 
 ");
 //etape 3 éxecution requête
-
 $query->execute();
 
 //etape 4 récuperation des lignes
-
 $cocktails=$query->fetchAll(PDO::FETCH_ASSOC);
 
 // Récupération de tous les cocktails stockés en base de données
